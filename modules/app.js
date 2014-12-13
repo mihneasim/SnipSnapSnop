f2d9847edb8614256921c434f6b86489b94b2a4f
Mihnea Simian <contact@mesimian.com>
2014-12-13 17:10:13 +0200

    the logicless views wired with state routes

--- modules/app.js
+++ modules/app.js
@@ -1,6 +1,39 @@
 angular.module('snipApp', [
     'ngResource', 'ui.router', 'ui.bootstrap', 'angular-lodash'
-]).config(function ( ) {
+]).config(function ($stateProvider, $urlRouterProvider) {
   'use strict';
+
+  $urlRouterProvider.otherwise('/404');
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'views/home.html'
+  })
+  .state('list', {
+    url: '/list',
+    templateUrl: 'views/list.html',
+    controller: function($scope) {
+      $scope.snippets = [{'id': '1', 'name': '1', 'description': 'some desc', 'code': 'echo "hello"', 'author': 'John Doe'}];
+    }
+  })
+  .state('create', {
+    url: '/create',
+    templateUrl: 'views/create.html',
+    controller: function($scope) {
+    }
+  })
+  .state('detail', {
+    url: '/detail/{snippetId:[0-9]+}',
+    templateUrl: 'views/detail.html',
+    controller: function($scope) {
+      $scope.snippet = {'name': '1', 'description': 'some desc', 'code': 'echo "hello"', 'author': 'John Doe'};
+    }
+  })
+
+  .state('404', {
+    url: '/404',
+    templateUrl: 'views/404.html'
+  })
 
 });
